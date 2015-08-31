dd30a8991071d74e6ac42ad1b3630100237447d8
Arpit Gandhi <arpit@walkover.in>
2015-08-31 17:52:55 +0530

    Resolve COnflicts

--- app/src/main/java/com/mandaptak/android/Login/WelcomeScreen1.java
+++ app/src/main/java/com/mandaptak/android/Login/WelcomeScreen1.java
@@ -8,24 +8,12 @@ import android.view.ViewGroup;
 
 import com.mandaptak.android.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * Use the {@link WelcomeScreen1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WelcomeScreen1 extends Fragment {
 
     public WelcomeScreen1() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment WelcomeScreen1.
-     */
     public static WelcomeScreen1 newInstance() {
         WelcomeScreen1 fragment = new WelcomeScreen1();
         return fragment;

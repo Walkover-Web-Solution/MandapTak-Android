1c8b09d954b7f25605987a43b33b79aba15509ae
Ayushi Akriti <ayushi_akriti@cat.com>
2015-10-20 17:59:39 +0530

    DAGGER | Akriti | Sample Component and module with lower scope.

---
+++ app/src/main/java/com/mandaptak/android/DetailsProfileFragmentModule.java
@@ -0,0 +1,23 @@
+package com.mandaptak.android;
+
+import com.mandaptak.android.EditProfile.DetailsProfileFragment;
+import com.mandaptak.android.Profile.Religion.ReligionController;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class DetailsProfileFragmentModule {
+
+	private DetailsProfileFragment detailsProfileFragment;
+
+
+	public DetailsProfileFragmentModule(DetailsProfileFragment detailsProfileFragment) {
+		this.detailsProfileFragment = detailsProfileFragment;
+	}
+
+	@Provides
+	@PerDetailsProfileFragment
+	ReligionController provideReligionController(){
+		return detailsProfileFragment;
+	}
+}

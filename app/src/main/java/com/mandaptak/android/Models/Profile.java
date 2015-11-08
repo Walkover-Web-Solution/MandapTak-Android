8b5ea6d9249d25d7a1d4a049c9852ef1683f4446
Rashi Agarwal <rashi@walkover.in>
2015-11-08 13:51:32 +0530

    created profile model

---
+++ app/src/main/java/com/mandaptak/android/Models/Profile.java
@@ -0,0 +1,12 @@
+package com.mandaptak.android.Models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@ParseClassName("Profile")
+public class Profile extends ParseObject {
+
+}
